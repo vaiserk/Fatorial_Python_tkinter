3c2c47c9fac5e17d546fd05e14951c4fc1414343
vaiserk <46816984+vaiserk@users.noreply.github.com>
2022-11-15 19:24:58 -0300

    mudanças na interface

--- fatorial.py
+++ fatorial.py
@@ -106,44 +106,44 @@ class Application():
                 
                 try:
                     self.a = Label(self.frame,text= f'a permutação é {int(perr(n,a,b,c))}', bd="4",bg="#F5FFFA",font="arial 10 ")
-                    self.a.place(relx=0.155,rely=0.28,relwidth=0.9,relheight=0.05)  
+                    self.a.place(relx=0.155,rely=0.28+0.05,relwidth=0.9,relheight=0.05)  
         
                 except:
                     self.a = Label(self.frame,text= f'{perr(n,a,b,c)}',          bd="4",bg="#F5FFFA",font="arial 10 ")
-                    self.a.place(relx=0.155,rely=0.28,relwidth=0.9,relheight=0.05)
+                    self.a.place(relx=0.155,rely=0.28+0.05,relwidth=0.9,relheight=0.05)
             except:
                 self.a = Label(self.frame,text= f'valores invalidos ou nulos',bd="4",bg="#F5FFFA",font="arial 10 ")
-                self.a.place(relx=0.155,rely=0.28,relwidth=0.9,relheight=0.05)
+                self.a.place(relx=0.155,rely=0.28+0.05,relwidth=0.9,relheight=0.05)
         
         self.lb_permrepeticao = Label(self.frame,text='P',bd="4",bg="#F5FFFA",font="arial 40 ")
-        self.lb_permrepeticao.place(relx=0.0,rely=0.20,relwidth=0.1,relheight=0.1)  
+        self.lb_permrepeticao.place(relx=0.0,rely=0.20+0.05,relwidth=0.1,relheight=0.1)  
         
         self.lb_permrepeticaoNl = Label(self.frame,text='n =',bd="4",bg="#F5FFFA",font="arial 20 ")
-        self.lb_permrepeticaoNl.place(relx=0.05,rely=0.28,relwidth=0.05,relheight=0.05)
+        self.lb_permrepeticaoNl.place(relx=0.05,rely=0.28+0.05,relwidth=0.05,relheight=0.05)
         
         self.lb_permrepeticaoN = Entry(self.frame,background="#F5FFFA",font="arial 20")
-        self.lb_permrepeticaoN.place(relx=0.095,rely=0.28,relwidth=0.05,relheight=0.05)
+        self.lb_permrepeticaoN.place(relx=0.1,rely=0.28+0.05,relwidth=0.05,relheight=0.05)
         
         self.lb_permrepeticaoAl = Label(self.frame,text='a=',bd="4",bg="#F5FFFA",font="arial 15 ")
-        self.lb_permrepeticaoAl.place(relx=0.07,rely=0.19,relwidth=0.05,relheight=0.05)  
+        self.lb_permrepeticaoAl.place(relx=0.07,rely=0.19+0.05,relwidth=0.05,relheight=0.05)  
         
         self.lb_permrepeticaoA = Entry(self.frame,background="#F5FFFA",font="arial 10")
-        self.lb_permrepeticaoA.place(relx=0.11,rely=0.20,relwidth=0.03,relheight=0.03)
+        self.lb_permrepeticaoA.place(relx=0.11,rely=0.20+0.05,relwidth=0.03,relheight=0.03)
         
         self.lb_permrepeticaoBl = Label(self.frame,text='b=',bd="4",bg="#F5FFFA",font="arial 15 ")
-        self.lb_permrepeticaoBl.place(relx=0.14,rely=0.19,relwidth=0.05,relheight=0.05)
+        self.lb_permrepeticaoBl.place(relx=0.14,rely=0.19+0.05,relwidth=0.05,relheight=0.05)
         
         self.lb_permrepeticaoB = Entry(self.frame,background="#F5FFFA",font="arial 10")
-        self.lb_permrepeticaoB.place(relx=0.18,rely=0.20,relwidth=0.03,relheight=0.03)
+        self.lb_permrepeticaoB.place(relx=0.18,rely=0.20+0.05,relwidth=0.03,relheight=0.03)
         
         self.lb_permrepeticaoCl = Label(self.frame,text='c=',bd="4",bg="#F5FFFA",font="arial 15 ")
-        self.lb_permrepeticaoCl.place(relx=0.21,rely=0.19,relwidth=0.05,relheight=0.05)
+        self.lb_permrepeticaoCl.place(relx=0.21,rely=0.19+0.05,relwidth=0.05,relheight=0.05)
         
         self.lb_permrepeticaoC = Entry(self.frame,background="#F5FFFA",font="arial 10")
-        self.lb_permrepeticaoC.place(relx=0.25,rely=0.20,relwidth=0.03,relheight=0.03)
+        self.lb_permrepeticaoC.place(relx=0.25,rely=0.20+0.05,relwidth=0.03,relheight=0.03)
         
         self.bt_calcularPR = Button(self.frame,text="Calcular",command=permRepeticao)
-        self.bt_calcularPR.place(relx=0.8,rely=0.19,relwidth=0.09,relheight=0.02)
+        self.bt_calcularPR.place(relx=0.8,rely=0.19+0.09,relwidth=0.09,relheight=0.02)
 
         #combinação simples  
         def combinação():
@@ -154,32 +154,32 @@ class Application():
                 p = int(p)
                 try:
                     self.a = Label(self.frame,text= f'a permutação é {int(comb(n, p))}',  bd="4",bg="#F5FFFA",font="arial 10 ")
-                    self.a.place(relx=0.155,rely=0.48,relwidth=0.9,relheight=0.05)
+                    self.a.place(relx=0.155,rely=0.48+0.12,relwidth=0.9,relheight=0.05)
                 except:
                     self.a = Label(self.frame,text= f'{comb(n, p)}', bd="4",bg="#F5FFFA",font="arial 10 ")
-                    self.a.place(relx=0.155,rely=0.48,relwidth=0.9,relheight=0.05)
+                    self.a.place(relx=0.155,rely=0.48+0.12,relwidth=0.9,relheight=0.05)
             except:
                 self.a = Label(self.frame,text= f'valores invalidos ou nulos', bd="4",bg="#F5FFFA",font="arial 10 ")
-                self.a.place(relx=0.155,rely=0.48,relwidth=0.9,relheight=0.05)
+                self.a.place(relx=0.155,rely=0.48+0.12,relwidth=0.9,relheight=0.05)
         
     
         self.lb_combC = Label(self.frame,text='C',bd="4",bg="#F5FFFA",font="arial 40 ")
-        self.lb_combC.place(relx=0.0,rely=0.40,relwidth=0.1,relheight=0.1)  
+        self.lb_combC.place(relx=0.0,rely=0.40+0.12,relwidth=0.1,relheight=0.1)  
         
         self.lb_combN = Label(self.frame,text='n =',bd="4",bg="#F5FFFA",font="arial 20 ")
-        self.lb_combN.place(relx=0.05,rely=0.48,relwidth=0.05,relheight=0.05)
+        self.lb_combN.place(relx=0.05,rely=0.48+0.12,relwidth=0.05,relheight=0.05)
         
         self.lb_combP = Label(self.frame,text='p=',bd="4",bg="#F5FFFA",font="arial 15 ")
-        self.lb_combP.place(relx=0.07,rely=0.39,relwidth=0.05,relheight=0.05)  
+        self.lb_combP.place(relx=0.07,rely=0.39+0.12,relwidth=0.05,relheight=0.05)  
         
         self.lb_combPE = Entry(self.frame,background="#F5FFFA",font="arial 10")
-        self.lb_combPE.place(relx=0.11,rely=0.40,relwidth=0.03,relheight=0.03)
+        self.lb_combPE.place(relx=0.11,rely=0.40+0.12,relwidth=0.03,relheight=0.03)
         
         self.lb_combCE = Entry(self.frame,background="#F5FFFA",font="arial 20")
-        self.lb_combCE.place(relx=0.095,rely=0.48,relwidth=0.05,relheight=0.05)
+        self.lb_combCE.place(relx=0.1,rely=0.48+0.12,relwidth=0.05,relheight=0.05)
         
         self.bt_calcularC = Button(self.frame,text="Calcular",command=combinação)
-        self.bt_calcularC.place(relx=0.8,rely=0.39,relwidth=0.09,relheight=0.02)
+        self.bt_calcularC.place(relx=0.8,rely=0.39+0.12,relwidth=0.09,relheight=0.02)
         
         
          #arranjo simples  
@@ -191,33 +191,33 @@ class Application():
                 p = int(p)
                 try:
                     self.a = Label(self.frame,text= f'a permutação é {int(arran(n, p))}',  bd="4",bg="#F5FFFA",font="arial 10 ")
-                    self.a.place(relx=0.155,rely=0.68,relwidth=0.9,relheight=0.05)
+                    self.a.place(relx=0.155,rely=0.68+0.17,relwidth=0.9,relheight=0.05)
                 except:
                     self.a = Label(self.frame,text= f'{arran(n, p)}', bd="4",bg="#F5FFFA",font="arial 10 ")
-                    self.a.place(relx=0.155,rely=0.68,relwidth=0.9,relheight=0.05)
+                    self.a.place(relx=0.155,rely=0.68+0.17,relwidth=0.9,relheight=0.05)
             except:
                 self.a = Label(self.frame,text= f'valores invalidos ou nulos', bd="4",bg="#F5FFFA",font="arial 10 ")
-                self.a.place(relx=0.155,rely=0.68,relwidth=0.9,relheight=0.05)
+                self.a.place(relx=0.155,rely=0.68+0.17,relwidth=0.9,relheight=0.05)
             
             
             
         self.lb_arrbA = Label(self.frame,text='A',bd="4",bg="#F5FFFA",font="arial 40 ")
-        self.lb_arrbA.place(relx=0.0,rely=0.60,relwidth=0.1,relheight=0.1)  
+        self.lb_arrbA.place(relx=0.0,rely=0.60+0.17,relwidth=0.1,relheight=0.1)  
         
         self.lb_arrbN = Label(self.frame,text='n =',bd="4",bg="#F5FFFA",font="arial 20 ")
-        self.lb_arrbN.place(relx=0.05,rely=0.68,relwidth=0.05,relheight=0.05)
+        self.lb_arrbN.place(relx=0.05,rely=0.68+0.17,relwidth=0.05,relheight=0.05)
         
         self.lb_arrbP = Label(self.frame,text='p=',bd="4",bg="#F5FFFA",font="arial 15 ")
-        self.lb_arrbP.place(relx=0.07,rely=0.59,relwidth=0.05,relheight=0.05)  
+        self.lb_arrbP.place(relx=0.07,rely=0.59+0.17,relwidth=0.05,relheight=0.05)  
         
         self.lb_arrbPE = Entry(self.frame,background="#F5FFFA",font="arial 10")
-        self.lb_arrbPE.place(relx=0.11,rely=0.60,relwidth=0.03,relheight=0.03)
+        self.lb_arrbPE.place(relx=0.11,rely=0.60+0.17,relwidth=0.03,relheight=0.03)
         
         self.lb_arrbNE = Entry(self.frame,background="#F5FFFA",font="arial 20")
-        self.lb_arrbNE.place(relx=0.095,rely=0.68,relwidth=0.05,relheight=0.05)
+        self.lb_arrbNE.place(relx=0.098,rely=0.68+0.17,relwidth=0.05,relheight=0.05)
         
         self.bt_calcularA = Button(self.frame,text="Calcular",command=arranjo)
-        self.bt_calcularA.place(relx=0.8,rely=0.59,relwidth=0.09,relheight=0.02)
+        self.bt_calcularA.place(relx=0.8,rely=0.59+0.17,relwidth=0.09,relheight=0.02)
          
          
          
